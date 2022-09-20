3d19f82e82b1a14dc365499af57cf9dd134aa084
mahmudulhaquequdrati <freelancermahmudqudrati@gmail.com>
2022-09-20 23:21:06 +0600

    server updated

--- controllers/authController.js
+++ controllers/authController.js
@@ -13,7 +13,7 @@ const login = asyncHandler(async (req, res) => {
       message: "All fields are required",
     });
   }
-  const foundUser = await user.findOne({ email }).exec();
+  const foundUser = await User.findOne({ email }).exec();
   if (!foundUser || !foundUser.active) {
     return res.status(401).json({
       message: "Unauthorized",
