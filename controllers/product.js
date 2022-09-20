9c67bd8bf1f931fb5c6cf1b028821296c5f6f97c
mahmudulhaquequdrati <freelancermahmudqudrati@gmail.com>
2022-09-20 23:30:56 +0600

    updated

--- controllers/product.js
+++ controllers/product.js
@@ -1,4 +1,4 @@
-const Product = require("../models/product");
+const Product = require("../models/Product");
 const asyncHandler = require("express-async-handler");
 const { Cursor } = require("mongoose");
 /* ============DONE========= */
